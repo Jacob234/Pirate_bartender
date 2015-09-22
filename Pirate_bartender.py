e444a80b416e447904c692c8d2a4b69ce3720230
Jacob Keller <jacobbenkell@gmail.com>
2015-09-22 01:25:48 +0000

    second commit

--- Pirate_bartender.py
+++ Pirate_bartender.py
@@ -1,3 +1,4 @@
+import random
 questions = {
     "strong": "Do ye like yer drinks strong?",
     "salty": "Do ye like it with a salty tang?",
@@ -13,89 +14,29 @@ ingredients = {
     "sweet": ["sugar cube", "spoonful of honey", "spash of cola"],
     "fruity": ["slice of orange", "dash of cassis", "apple on top"]
 }
-
+Drink = []
 Tastes = { }
-
+Prefer = [ ]
 def pirate ():
     """I will ask what drinks you like"""
-    Tastes.update(ingredients)
     
     print "I'm a {}, I will ask you what you like".format(pirate.__name__)
-    Strong = raw_input(questions["strong"])
-    if Strong == "yes" or Strong == "y":
-        L_Strong=True
-    else:
-        L_Strong = False
-    if L_Strong == False:
-       del Tastes["strong"]
-    
-    Salty = raw_input(questions["salty"])
-    if Salty == "yes" or Salty == "y":
-        L_Salty=True
-    else:
-        L_Salty = False
-    if L_Salty == False:
-        del Tastes["salty"]
-    
-    Bitter = raw_input(questions["bitter"])
-    if Bitter == "yes" or Bitter == "y":
-        L_Bitter=True
-    else:
-        L_Bitter = False
-    if L_Bitter == False:
-        del Tastes["bitter"]
    
-    Sweet = raw_input(questions["sweet"])
-    if Sweet == "yes" or Sweet == "y":
-        L_Sweet=True
-    else:
-        L_Sweet = False
-    if L_Sweet == False:
-        del Tastes["sweet"]
-    
-        
-    Fruity = raw_input(questions["fruity"])
-    if Fruity == "yes" or Fruity == "y":
-        L_Fruity=True
-    else:
-        L_Fruity = False
-    if L_Fruity == False:
-        del Tastes["fruity"]
-    
-    return Tastes
-    
-    
-import random
-    
-Drink = []
+    for taste in questions:
+        print questions[taste]
+        x =raw_input().lower()
+        if x == "y" or x == "yes":
+            Prefer.append(taste)
+        print Prefer
+
 
 def drink():
     print "Yarr now we will see what {} I can scavenge up from the poop deck, land lubber".format(drink.__name__)
-    
-    
-    if "strong" in Tastes == True:
-        for strong in Tastes:
-            Drink.append(strong)
-            print strong
-    if "salty" in Tastes == True:
-        Salt = random.choice(Tastes)
-        print Salt
-        Drink.append(Salt)
-    if "bitter" in Tastes == True:
-        Bitt = random.choice("bitter")
-        print Bitt
-        Drink.append(Bitt)
-    if "sweet" in Tastes == True:
-        Swe = random.choice("sweet")
-        print Swe
-        Drink.append(Swe)
-    if "fruity" in Tastes == True:
-        Fru = random.choice("fruity")
-        print Fru
-        Drink.append(Fru)
-    
-    print random.choice(Drink)
-    
+    for taste in Prefer:
+        Drink.append(random.choice(ingredients[taste]))
+    print "Here it is ye scally wag"
+    for ingred in Drink:
+        print "A {}".format(ingred)
     
 
 if __name__ == '__main__':
